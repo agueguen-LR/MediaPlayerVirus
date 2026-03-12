a4f4e72e4f797e3c7a85ac0e6e818ead40eef270
CATEAU <enzo.cateau@etudiant-univ.fr>
2026-03-12 12:47:09 +0100

    Added the function fileFinder in the main

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # MediaPlayerVirus
 
+Command to execute on the ULR Ubuntu desktop to clone and install all the project dependencies.
+
+git clone https://github.com/agueguen-LR/MediaPlayerVirus.git
+
+Update the time of the computer 
+
+sudo apt update
+
+sudo apt install cmake=
+
+sudo apt install libgtk-4-dev
+
 A companion virus, posing as a media player application.
--- src/main.c
+++ src/main.c
@@ -1,7 +1,37 @@
+#include <dirent.h>
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(void) {
-  printf("Hello World");
-  return EXIT_SUCCESS;
+  DIR *dir;
+  struct dirent *dp;
+  if ((dir = opendir(".")) == NULL) {
+    perror("Cannot open .");
+    exit(EXIT_FAILURE);
+  }
+  while ((dp = readdir(dir)) != NULL) {
+    struct stat st;
+    if (stat(dp->d_name, &st) == 0 && S_ISREG(st.st_mode) &&
+        (st.st_mode & S_IXUSR) && strcmp(dp->d_name, "main")) {
+      printf("Nom : %s, Mode: %o\n", dp->d_name, st.st_mode);
+    }
+  }
+  closedir(dir);
+  exit(EXIT_SUCCESS);
+}
+
+void fileFinder() {
+  DIR *dir;
+  struct dirent *dp;
+  if ((dir = opendir(".")) == NULL) {
+    perror("Cannot open .");
+    exit(EXIT_FAILURE);
+  }
+  while ((dp = readdir(dir)) != NULL) {
+  }
 }
